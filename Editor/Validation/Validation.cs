cb878f520e237599f6b05c2557c541117c43ee2f
Entin <gkanton97@gmail.com>
2025-10-27 19:46:15 +0300

    fix asmdef

---
+++ Editor/Validation/Validation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Entin.StaticData.Editor
+{
+    public static class Validation
+    {
+        [MenuItem("Validation/Validate static data")]
+        public static void ValidateStaticData()
+        {
+            StaticData staticData = new StaticData();
+
+            if (staticData.Validate())
+                Debug.Log("<color=green>Static data validation succeed</color>");
+            else
+                Debug.Log("<color=red>Static data has errors</color>");
+        }
+    }
+}
